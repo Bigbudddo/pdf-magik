37d8ac577a7ec520b58e478105201991cdf1a0af
Stuart Harrison <stuart.harrison@saveandinvest.co.uk>
2018-05-22 11:14:19 +0100

    setup the project basic files and references

---
+++ PdfMagikLITE/PDF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+
+namespace PdfMagikLITE {
+    
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".pdf")]
+    public class PDF : SharpContextMenu {
+
+        
+    }
+}
